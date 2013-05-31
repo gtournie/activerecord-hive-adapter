1c5aa052184bfbc73ba7b0a6cad4cfdfc74d37ea
Aram Grigoryan <aram@stackpop.com>
2013-05-31 11:21:40 -0400

    Removed call to cache.  To be revisited.

--- lib/activerecord-hive-adapter.rb
+++ lib/activerecord-hive-adapter.rb
@@ -6,14 +6,7 @@ if defined?(::Rails::Railtie)
       class HiveRailtie < ::Rails::Railtie
         ActiveSupport.on_load(:active_record) do
           require 'active_record/connection_adapters/hive_adapter'
-
-          # Cache column descriptions between requests in test and production environments
-          if Rails.env.test? || Rails.env.production?
-            ActiveRecord::ConnectionAdapters::HiveAdapter.cache_columns = true
-          end
-
         end
-
       end
     end
   end
